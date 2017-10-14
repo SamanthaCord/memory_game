43d45878c6400e3bb8803a830f0315ce4aa97c7d
Samantha <sam@cordandberg.com>
2017-10-14 11:03:22 +1100

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,28 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+var cardId = cards[0,1,2,3];
 
-var cardOne = cards[0];
-
-
-cardsInPlay.push('cardOne');
-
-console.log("User flipped Queen");
-
-var cardTwo = cards[2];
-
-
-cardsInPlay.push('cardTwo');
-
-console.log("User flipped King");
-
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+var checkForMatch = function () {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
 }
-else {
-	alert("Sorry, try again.");
-}
+};
+
+var flipCard = function (cardId) {
+console.log("User flipped " + cards[cardId]);
+{
+cardsInPlay.push(cards[cardId]);
+};
+};
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
+
+
+
+
 
